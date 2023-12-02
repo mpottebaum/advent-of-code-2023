e96dc673a872d54c4e4221a4cf963d3e8eb9f5f7
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-12-02 12:18:48 -0600

    day02 part one

--- day02/solution.go
+++ day02/solution.go
@@ -1,9 +1,55 @@
 package day02
 
 import (
+	"aoc/utils"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
+var ElfBag = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
 func Solve() {
-	fmt.Println("day 2 babyyyyy let us go")
+	input := utils.ReadFileToString("day02/input.txt")
+	games := strings.Split(input, "\n")
+
+	sum := 0
+	for iG := 0; iG < len(games); iG++ {
+		game := games[iG]
+		idAndHandfuls := strings.Split(game, ": ")
+		handfulsStr := idAndHandfuls[1]
+		handfuls := strings.Split(handfulsStr, "; ")
+
+		valid := true
+	handfulCheck:
+		for iH := 0; iH < len(handfuls); iH++ {
+			handful := handfuls[iH]
+			cubeCountStrs := strings.Split(handful, ", ")
+			for iC := 0; iC < len(cubeCountStrs); iC++ {
+				cubeCountStr := cubeCountStrs[iC]
+				countAndColor := strings.Split(cubeCountStr, " ")
+				count, countParseErr := strconv.ParseInt(countAndColor[0], 10, 64)
+				color := countAndColor[1]
+				colorTotal, colorOk := ElfBag[color]
+				if countParseErr == nil && colorOk {
+					if int(count) > colorTotal {
+						valid = false
+						break handfulCheck
+					}
+				}
+			}
+		}
+		if valid {
+			gameAndId := strings.Split(idAndHandfuls[0], " ")
+			id, idParseErr := strconv.ParseInt(gameAndId[1], 10, 64)
+			if idParseErr == nil {
+				sum += int(id)
+			}
+		}
+	}
+	fmt.Println("Sum of all valid game IDs: ", sum)
 }
