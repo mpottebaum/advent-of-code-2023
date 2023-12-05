c017a416bf8fd8ac08664e1cfd969117db19a907
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-12-05 15:59:54 -0600

    day05 part two

--- day05/solution.go
+++ day05/solution.go
@@ -6,19 +6,7 @@ import (
 	"strings"
 )
 
-func Solve(inputFile string) {
-	fileName := "input"
-	if len(inputFile) > 0 {
-		fileName = inputFile
-	}
-	input := utils.ReadFileToString("day05/" + fileName + ".txt")
-
-	// split input string into: seeds list + all maps data
-	splitness := strings.Split(input, "\n")
-	seedsStr := splitness[0]
-	// compile slice of maps data
-	// structure == [[slice of map range data [num, num, num]]]
-	// [][][num, num, num]
+func ParseMapsDataSlice(splitness []string) [][][3]int {
 	mapsDataSlice := [][][3]int{}
 	currentMapIndex := -1
 	for i := 1; i < len(splitness); i++ {
@@ -45,36 +33,62 @@ func Solve(inputFile string) {
 			}
 		}
 	}
+	return mapsDataSlice
+}
+
+func MapSeedToLocation(seedNum int, mapsDataSlice [][][3]int) int {
+	num := seedNum
+	for mapI := 0; mapI < len(mapsDataSlice); mapI++ {
+		// for each map
+		mapness := mapsDataSlice[mapI]
+		// find all source ranges
+		for mapDatumI := 0; mapDatumI < len(mapness); mapDatumI++ {
+			mapDatum := mapness[mapDatumI]
+			sourceRangeStart := mapDatum[1]
+			rangeLength := mapDatum[2]
+			// if num fall within a range, use map to find new num
+			if num >= sourceRangeStart && num < sourceRangeStart+rangeLength {
+				destinationRangeStart := mapDatum[0]
+				distanceFromRangeStart := num - sourceRangeStart
+				num = destinationRangeStart + distanceFromRangeStart
+				break
+			}
+			// else num stays the same
+		}
+	}
+	return num
+}
+
+func Solve(inputFile string) {
+	fileName := "input"
+	if len(inputFile) > 0 {
+		fileName = inputFile
+	}
+	input := utils.ReadFileToString("day05/" + fileName + ".txt")
+
+	// split input string into: seeds list + all maps data
+	splitness := strings.Split(input, "\n")
+	seedsStr := splitness[0]
+	// compile slice of maps data
+	mapsDataSlice := ParseMapsDataSlice(splitness)
+
 	// search through seeds to find lowest location num
 	seedNumStrs := strings.Split(seedsStr, " ")
 	lowestLocationNum := -1
 	// start at index 1 b/c 0 is "seeds:" str
-	for sI := 1; sI < len(seedNumStrs); sI++ {
-		seedNumStr := seedNumStrs[sI]
-		seedNum, numParseErr := utils.ParseInt(seedNumStr)
-		if numParseErr == nil {
-			num := seedNum
-			for mapI := 0; mapI < len(mapsDataSlice); mapI++ {
-				// for each map
-				mapness := mapsDataSlice[mapI]
-				// find all source ranges
-				for mapDatumI := 0; mapDatumI < len(mapness); mapDatumI++ {
-					mapDatum := mapness[mapDatumI]
-					sourceRangeStart := mapDatum[1]
-					rangeLength := mapDatum[2]
-					// if num fall within a range, use map to find new num
-					if num >= sourceRangeStart && num < sourceRangeStart+rangeLength {
-						destinationRangeStart := mapDatum[0]
-						distanceFromRangeStart := num - sourceRangeStart
-						num = destinationRangeStart + distanceFromRangeStart
-						break
-					}
-					// else num stays the same
+	for sI := 1; sI < len(seedNumStrs); sI += 2 {
+		seedRangeStartStr := seedNumStrs[sI]
+		seedRangeLengthStr := seedNumStrs[sI+1]
+		seedRangeStart, startParseErr := utils.ParseInt(seedRangeStartStr)
+		seedRangeLength, lengthParseErr := utils.ParseInt(seedRangeLengthStr)
+		if startParseErr == nil && lengthParseErr == nil {
+			fmt.Println("seed range: ", seedRangeStart, seedRangeLength)
+			for seedNum := seedRangeStart; seedNum < seedRangeStart+seedRangeLength; seedNum++ {
+				locationNum := MapSeedToLocation(seedNum, mapsDataSlice)
+				// if num is less than lowest so far, replace lowest so far
+				if lowestLocationNum == -1 || (lowestLocationNum >= 0 && locationNum < lowestLocationNum) {
+					lowestLocationNum = locationNum
 				}
-			}
-			// if num is less than lowest so far, replace lowest so far
-			if lowestLocationNum == -1 || (lowestLocationNum >= 0 && num < lowestLocationNum) {
-				lowestLocationNum = num
 			}
 		}
 	}
