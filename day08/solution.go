b30131dc0413add3386aef4862ccd6130b124c27
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-12-08 20:26:07 -0600

    day08 part two

--- day08/solution.go
+++ day08/solution.go
@@ -19,8 +19,8 @@ func Solve(inputFile string) {
 	input := utils.ReadFileToString("day08/" + fileName + ".txt")
 	rows := strings.Split(input, "\n")
 	stepsStr := rows[0]
+	startEls := []string{}
 
-	// collect all els + instrs in map
 	elMap := map[string]Instruct{}
 	for i := 2; i < len(rows); i++ {
 		row := rows[i]
@@ -30,24 +30,50 @@ func Solve(inputFile string) {
 		left := string([]rune(instrStr)[1:4])
 		right := string([]rune(instrStr)[6:9])
 		elMap[el] = Instruct{Left: left, Right: right}
-	}
-	// walk the steps until we get to ZZZ
-	steps := strings.Split(stepsStr, "")
-	currEl := "AAA"
-	numSteps := 0
-	for currEl != "ZZZ" {
-		for i := 0; i < len(steps); i++ {
-			numSteps++
-			instruct := elMap[currEl]
-			if step := steps[i]; step == "L" {
-				currEl = instruct.Left
-			} else {
-				currEl = instruct.Right
-			}
-			if currEl == "ZZZ" {
-				break
-			}
+		ender := rune(el[2])
+		if ender == 'A' {
+			startEls = append(startEls, el)
 		}
 	}
+
+	steps := strings.Split(stepsStr, "")
+	loopNums := []int{}
+	for i := 0; i < len(startEls); i++ {
+		startEl := startEls[i]
+		el := startEl
+		numSteps := 0
+		steppedEls := map[string]bool{}
+		var startLoopEl string
+	stepper:
+		for {
+			for iStep := 0; iStep < len(steps); iStep++ {
+				step := steps[iStep]
+				instruct := elMap[el]
+				if step == "L" {
+					el = instruct.Left
+				} else {
+					el = instruct.Right
+				}
+
+				if el == startLoopEl {
+					numSteps++
+					break stepper
+				}
+				if len(startLoopEl) > 0 {
+					numSteps++
+				} else {
+					_, steppedUpon := steppedEls[el]
+					ender := rune(el[2])
+					if steppedUpon && ender == 'Z' {
+						startLoopEl = el
+					} else {
+						steppedEls[el] = true
+					}
+				}
+			}
+		}
+		loopNums = append(loopNums, numSteps)
+	}
+	numSteps := utils.LCM(loopNums[0], loopNums[1], loopNums[2:]...)
 	fmt.Println("Number or required steps: ", numSteps)
 }
